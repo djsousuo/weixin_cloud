b50892c245a737da5ebde37e53ac2ae379d18128
djsousuo <35257733+djsousuo@users.noreply.github.com>
2022-03-15 18:01:37 +0800

    Update helloworld.go

--- helloworld.go
+++ helloworld.go
@@ -2,30 +2,19 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	client:=&http.Client{}
-	req, _ := http.NewRequest("GET", "http://ip.qw0.cc", nil)
-	req.Header.Set("User-Agent","curl/7.64.1")
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Print("请求失败: " + err.Error())
-	}
-	respBody,_:=ioutil.ReadAll(resp.Body)
-	target := string(respBody)
-	w.Header().Set("Content-Type","application/json")
-	fmt.Fprintf(w, "%s!\n", target)
+	fmt.Fprintf(w, "111111")
 }
 
 func main() {
 	http.HandleFunc("/", handler)
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8012"
+		port = "80"
 	}
 	http.ListenAndServe("0.0.0.0:"+port, nil)
 }
